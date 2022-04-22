4dbd4a6d0853b79bc4245fb6061ed06d9d9b68cd
Daniel Froes <danielfroestbf@gmail.com>
2022-04-22 17:36:45 -0300

    feat: adicionando arquivo JavaScript

---
+++ index.js
@@ -0,0 +1,25 @@
+let departureDateEntry = prompt("Digite a data de partida (formata DD/MM/YYYY)");
+
+let departureDate = moment(departureDateEntry, "DD/MM/YYYY");
+
+let today = moment();
+
+let dateDiff = today - departureDate;
+
+let chooseOption = prompt("Escolha como gostaria de exibir o tempo de partida\n 1 - Segundos\n 2 - Minutos \n 3 - horas\n 4 - Dias")
+
+if(chooseOption == "1"){
+    let secondsOfDeparture = Math.round(dateDiff / 1000)
+    alert("Tempo de vôo " + secondsOfDeparture + "segundos")
+}else if( chooseOption == "2"){
+    let minutesOfDeparture = Math.round(dateDiff / 1000 / 60);
+    alert("Tempo de vôo " + minutesOfDeparture + "minutos")
+}else if( chooseOption == "3"){
+    let hoursOfDeparture = Math.round(dateDiff / 1000 / 3600);
+    alert("Tempo de vôo " + hoursOfDeparture + "horas")
+}else if( chooseOption == "4"){
+    let daysOfDeparture = Math.round(dateDiff / 1000 / 3600 / 24);
+    alert("Tempo de vôo " + daysOfDeparture + "dias")
+} else{
+    alert("Opção invalida")
+}
